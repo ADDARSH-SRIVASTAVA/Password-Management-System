78c5cfd509aae4ffe09def00661c9ef3a6492015
Addarsh Srivastava <addarshsri@gmail.com>
2020-12-30 19:32:34 +0530

    Updated

---
+++ routes/index.js
@@ -0,0 +1,160 @@
+var express = require('express');
+var router = express.Router();
+var userModule=require('../modules/user');
+var passCatModel = require('../modules/password_category');
+var passModel = require('../modules/add_password');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
+var getPassCat = passCatModel.find({});
+var getAllPass = passModel.find({});
+/* GET home page. */
+
+function checkLoginUser(req,res,next){
+  var userToken=localStorage.getItem('userToken');
+  try {
+    var decoded = jwt.verify(userToken, 'loginToken');
+  } catch(err) {
+    res.redirect('/');
+  }
+  next();
+}
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+function checkUsername(req,res,next){
+  var uname=req.body.uname;
+  var checkexistemail=userModule.findOne({username:uname});
+  checkexistemail.exec((err,data)=>{
+    if(err) throw err;
+    if(data){
+      return  res.render('signup', { title: 'Password Management System', msg:'Username already Exist'});
+    }
+    next();
+  });
+}
+function checkEmail(req,res,next){
+  var email=req.body.email;
+  var checkexistemail=userModule.findOne({email:email});
+  checkexistemail.exec((err,data)=>{
+    if(err) throw err;
+    if(data){
+      return  res.render('signup', { title: 'Password Management System', msg:'Email already Exist'});
+    }
+    next();
+  });
+} 
+
+
+
+router.get('/', function(req, res, next) {
+  
+  var loginUser=localStorage.getItem('loginUser');
+  if(loginUser){
+    res.redirect('./dashboard');
+  }else{
+  res.render('index', { title: 'Password Management System', msg:'' });
+  }
+});
+
+router.post('/', function(req, res, next) {
+  var username=req.body.uname; 
+  var password=req.body.password;
+  var checkUser=userModule.findOne({username:username});
+  checkUser.exec((err,data)=>{
+    if(err) throw err;
+    var getUserID=data._id; 
+    var getPassword=data.password; 
+    if(bcrypt.compareSync(password,getPassword)){
+      var token = jwt.sign({ userID: getUserID }, 'loginToken');
+      localStorage.setItem('userToken', token);
+      localStorage.setItem('loginUser', username);
+      res.redirect('/dashboard');
+    }else{
+      res.render('index', { title: 'Password Management System',msg:'Invalid Username And Password' });
+    }
+    
+  });
+  
+});
+
+
+router.get('/signup', function(req, res, next) {
+  var loginUser=localStorage.getItem('loginUser');
+  if(loginUser){
+    res.redirect('./dashboard');
+  }else{
+  res.render('signup', { title: 'Password Management System', msg:'' });
+  }
+});
+router.post('/signup',checkUsername, checkEmail, function(req, res, next) {
+
+  var username=req.body.uname;
+  var email=req.body.email; 
+  var password=req.body.password;
+  var confpassword=req.body.confpassword;
+  if(password!=confpassword){
+    res.render('signup', { title: 'Password Management System', msg:'Password Not Matched!' });
+  }else{
+
+    password =bcrypt.hashSync(req.body.password,10);
+  
+  var userDetails=new userModule({
+    username:username,
+    email:email,
+    password:password
+  });
+  userDetails.save((err,doc)=>{
+    if(err) throw err;
+    res.render('signup', { title: 'Password Management System', msg:'User Registered Successfully' });
+
+  });
+}
+  
+});
+
+/** 
+router.get('/view-all-password', checkLoginUser, function(req, res, next) {
+  var loginUser=localStorage.getItem('loginUser');
+  var perPage = 1;
+  var page = req.params.page || 1;
+  getAllPass.skip((perPage * page) - perPage)
+  .limit(perPage).exec(function(err,data){
+    if(err) throw err;
+    passModel.countDocuments({}).exec((err,count)=>{  
+    res.render('view-all-password', { title: 'Password Management System' , loginUser: loginUser, records: data,
+    records: data,
+    current: page,
+    pages: Math.ceil(count / perPage)
+  });
+  });
+});
+});
+router.get('/view-all-password/:page', checkLoginUser, function(req, res, next) {
+  var loginUser=localStorage.getItem('loginUser');
+  var perPage = 1;
+  var page = req.params.page || 1;
+  getAllPass.skip((perPage * page) - perPage)
+  .limit(perPage).exec(function(err,data){
+    if(err) throw err;
+    passModel.countDocuments({}).exec((err,count)=>{  
+    res.render('view-all-password', { title: 'Password Management System' , loginUser: loginUser, records: data,
+    records: data,
+    current: page,
+    pages: Math.ceil(count / perPage)
+  });
+  });
+});
+});
+**/
+
+router.get('/logout', function(req, res, next) {
+  localStorage.removeItem('userToken');
+  localStorage.removeItem('loginUser');
+  res.redirect('/');
+});
+
+module.exports = router;
